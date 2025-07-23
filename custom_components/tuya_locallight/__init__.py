80556ed78135425a453bd6b756284e867fd8e3fa
spbdimka <dimka.slash@gmail.com>
2025-07-23 20:39:18 +0300

    Fix await

--- custom_components/tuya_locallight/__init__.py
+++ custom_components/tuya_locallight/__init__.py
@@ -4,9 +4,7 @@ async def async_setup(hass, config):
     return True
 
 async def async_setup_entry(hass, entry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "light")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "light")
     return True
 
 async def async_unload_entry(hass, entry):

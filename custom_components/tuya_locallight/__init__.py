b53cb23b25fc2dca4b88bdf4a396fa3f47285f95
spbdimka <dimka.slash@gmail.com>
2025-07-23 21:24:55 +0300

    Fix init

--- custom_components/tuya_locallight/__init__.py
+++ custom_components/tuya_locallight/__init__.py
@@ -1,9 +1,10 @@
 from .const import DOMAIN
-
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 async def async_setup(hass, config):
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, ["light"])
     return True
 
